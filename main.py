aea4e1a049f56bd788e1c59511a9298805d91096
taeil <jty0234@gmail.com>
2022-07-02 15:32:33 +0900

    Feat: mysql 연결 설정 추가

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from router.main import main
 from router.user import user
+from engine.mysql import mysql_connection_pool
+from settings import get_config
 
 app = Flask(__name__)
 
@@ -9,4 +11,6 @@ app.register_blueprint(user)
 
 
 if __name__ == '__main__':
+    config = get_config()
+    mysql_connection_pool.setup(config=config)
     app.run()
